38195ec54763992e99758968e136979d9413c8cf
Joshua Israel Albao <albao.joshuaisrael@gmail.com>
2023-05-12 00:07:28 +0800

    Added initial UI layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { Button, Form, InputGroup } from 'react-bootstrap'
 import styles from './App.module.scss'
 
 function App() {
   return (
     <div className={styles.App}>
-      Hello World
+      <main>
+        <h1>Base64 Encoder</h1>
+        <InputGroup className="mb-3">
+          <Form.Control as="textarea" aria-label="With textarea" readOnly />
+        </InputGroup>
+        <InputGroup>
+          <Form.Control
+            placeholder="Please enter any text"
+            aria-label="Text"
+            aria-describedby="basic-addon1"
+          />
+          <Button>
+            Convert
+          </Button>
+        </InputGroup>
+      </main>
     </div>
   )
 }
