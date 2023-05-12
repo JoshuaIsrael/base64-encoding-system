ae3a64b7ce89a7fc36879458cf6a5eccaa8e9df0
Joshua Israel Albao <albao.joshuaisrael@gmail.com>
2023-05-13 07:56:01 +0800

    Added initial encode request using WebSocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
+import { FormEvent, useState } from 'react'
 import { Button, Form, InputGroup } from 'react-bootstrap'
+import { encodeString } from './services/encode';
 import styles from './App.module.scss'
 
 function App() {
+  const [encodedString, setEncodedString] = useState<string>('');
+
+  const onReceiveText = (value: string) => {
+    setEncodedString(prev => prev += value);
+  }
+
+  const onConvert = (event: FormEvent) => {
+    event.preventDefault();
+    const inputElement = (event.target as HTMLFormElement)[0] as HTMLInputElement;
+    encodeString(inputElement.value, onReceiveText);
+  }
+
   return (
     <div className={styles.App}>
       <main>
         <h1>Base64 Encoder</h1>
         <InputGroup className="mb-3">
-          <Form.Control as="textarea" aria-label="With textarea" readOnly />
-        </InputGroup>
-        <InputGroup>
           <Form.Control
-            placeholder="Please enter any text"
-            aria-label="Text"
-            aria-describedby="basic-addon1"
+            as="textarea"
+            aria-label="Encoded Text"
+            readOnly
+            value={encodedString}
           />
-          <Button>
-            Convert
-          </Button>
         </InputGroup>
+        <Form onSubmit={onConvert}>
+          <InputGroup>
+            <Form.Control
+              placeholder="Please enter any text"
+              aria-label="Text"
+              aria-describedby="basic-addon1"
+            />
+            <Button type="submit">
+              Convert
+            </Button>
+          </InputGroup>
+        </Form>
       </main>
     </div>
   )
---
+++ src/services/encode.ts
@@ -0,0 +1,24 @@
+const connectionUrl = "ws://localhost:5000/ws"
+
+export const encodeString = async (input: string, onReceive: (value: string) => void): Promise<void> => {
+  const socket = new WebSocket(connectionUrl);
+  socket.onopen = function (event) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    var data = input;
+    socket.send(data);
+  };
+  socket.onclose = function (event) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    socket.close(1000, "Closing from client");
+  };
+  socket.onerror = function (event) {
+    console.log(event)
+  };;
+  socket.onmessage = function (event) {
+    onReceive(event.data);
+  };
+};
